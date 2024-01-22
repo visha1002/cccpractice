7d2ba8f5ea37f70c4527722048d23c39c1bf8916
Visha <vishadobariya1002@gmail.com>
2024-01-22 15:03:41 +0530

    use recursive function in fibonacci

--- string_exercise_9.php
+++ string_exercise_9.php
@@ -1,27 +1,17 @@
 <?php
-    function fibonacci($num){
-        $num1 = 0;
-        $num2 = 1;
-        echo "Fibbonacci series upto $num terms are : <br>";
 
-        if($num == 1){
-            echo $num1;
-        }
-        else if($num == 2){
-            echo "$num1 $num2"; 
+    $num = 10;
+    function fibonacci($num){
+        if($num <= 1){
+            return $num;
         }
         else{
-            echo "$num1 $num2 ";
-            for($i = 1; $i <= $num-2; $i++){
-                $num3 = $num1 + $num2;
-                echo $num3." ";
-        
-                $num1 = $num2;
-                $num2 = $num3;
-            }
+            return fibonacci($num-1) + fibonacci($num-2);
         }
     }
     
-    fibonacci(12);
-    
+    for($i=0; $i<$num; $i++){
+        echo fibonacci($i)," ";
+    }
+
 ?>
