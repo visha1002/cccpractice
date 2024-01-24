e48425aacbf5a5bca4c5c3f6f47df97002fffc9f
Visha <vishadobariya1002@gmail.com>
2024-01-24 09:33:50 +0530

    change logic of pattern

--- pattern.php
+++ pattern.php
@@ -1,10 +1,25 @@
 <?php
 $n=10;
+
+//first logic
 for($i=1;$i<=$n;$i++){
 
     for($j=1;$j<=$n;$j++){
-        if($j<=($n+1)-$i){
-        echo $j." ";
+        if($i + $j >= $n+2){
+        echo "&nbsp";
+        }
+        else{
+            echo $j." ";
+        }
+    }
+    echo "<br>";
+}
+// second logic
+for($i=1;$i<=$n;$i++){
+
+    for($j=1;$j<=$n;$j++){
+        if($j<= ($n+1)-$i){
+            echo $j." ";
         }
     }
     echo "<br>";
