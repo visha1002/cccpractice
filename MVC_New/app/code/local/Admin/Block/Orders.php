878fccbd6842309844e3bb4258e6e1826757f1d0
Visha <vishadobariya1002@gmail.com>
2024-03-20 18:38:53 +0530

    created orders for admin

--- MVC_New/app/code/local/Admin/Block/Orders.php
+++ MVC_New/app/code/local/Admin/Block/Orders.php
@@ -7,4 +7,12 @@ class Admin_Block_Orders extends Core_Block_Template
         $this->setTemplate('sales/order/admin/view.phtml');
     }
 
+    public function getCancelReqestOrders()
+    {
+        $cancel = Mage::getModel('sales/order')->getCollection()->addFieldToFilter('status', 'can-Request');
+        // echo "<pre>";
+        // print_r($cancel->getData());
+        return $cancel;
+    }
+
 }
